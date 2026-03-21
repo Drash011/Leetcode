761c5472eeb2d826365c58f8238ae507131e5e80
Drashti Thummar <thummard097@gmail.com>
2026-03-21 13:35:20 +0530

    Arrays

---
+++ q-15-1D_min_max.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int a[size];
+
+    printf("\n\nArray Input\n\n");
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter element a [%d] : ", i);
+        scanf("%d", &a[i]);
+    }
+
+    int max = a[0], min = a[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (a[i] > max)
+            max = a[i];
+
+        if (a[i] < min)
+            min = a[i];
+    }
+
+    printf("\n\nArray Output\n\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+
+    printf("\n\nMaximum number of Array Elements: %d\n", max);
+
+    printf("\n\nMinimum number of Array Elements: %d\n", min);
+}
---
+++ q-16-1D_2max.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int a[size];
+
+    printf("\n\nArray Input\n\n");
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter element a [%d] : ", i);
+        scanf("%d", &a[i]);
+    }
+
+    int max = a[0], max2 = max;
+    for (int i = 0; i < size; i++)
+    {
+        if (a[i] > max)
+            max = a[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        if (a[i] > max2 && a[i] < max)
+        {
+            max2 = a[i];
+        }   
+    }    
+
+    printf("\n\nArray Output\n\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+
+
+    printf("\n\nMaximum number of Array Elements: %d\n", max);
+    printf("\n\nSecond Maximum number of Array Elements: %d\n", max2);
+
+}
